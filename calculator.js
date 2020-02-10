42cff7b68e2c4cd5771dbc5a5119308b8cc75498
andersonl1208 <andersonl1208a@gmail.com>
2020-02-09 20:13:11 -0600

    Created initial parse tree function and evaluation function

--- calculator.js
+++ calculator.js
@@ -190,10 +190,16 @@ function processFunction() {
 
     document.getElementById("result").innerHTML = implicitMultiplicationString;
 
+    if(!validateProperFunction(implicitMultiplicationString)) {
+        console.error("Invalid function!");
+    }
+    else {
+        console.log(evaluateFunction(createParseTree(implicitMultiplicationString), 1));
+    }
+
     //convert to lowercase? Depends on if we replace things like cos() with some temporary var
 }
 
-const nonTerminalList = ['S', 'A', 'T', 'B', 'R'];
 const terminalList = ['+', '-', '*', '/', 'x', '(', ')', '$'];
 
 const map = {
@@ -214,7 +220,7 @@ function validateProperFunction(func) {
     let input = func.replace(numberRemover, "x");
 
     let invalidCharacters = /[^+\-*/x()]/g;
-    if (input.search(invalidCharacters) != -1) {
+    if (input.length == 0 || input.search(invalidCharacters) != -1) {
         return false;
     }
 
@@ -232,8 +238,6 @@ function validateProperFunction(func) {
 				//break;
             //}
         let currentElem = charStack.pop();
-        let terminalLocation = input.search();
-        let nonTerminalLocation = input.search();
 
         while(!terminalList.includes(currentElem)) {
             if(map[currentElem][input[point]] == "!") {
@@ -260,3 +264,231 @@ function validateProperFunction(func) {
 
     return !charStack.length && !failed;
 }
+
+/*
+ * 
+ * @param {string} input 
+ */
+//function getNextToken(input) {
+    
+//    let tokenRetriever = /(\d+(\.\d+)?|[+\-*/x()])/; // This may need additional work. It fails for .32 or something similar (must do 0.32).
+//    input = input.replace(tokenRetriever, (matchedString, savedToken) => { token = savedToken; return "";});
+
+//    return {"input": input, "token": token};
+//}
+
+const TokenType = Object.freeze({OPEN_PARANTHESES: 1, CLOSE_PARANTHESES: 2, VALUE: 3, ADDITION: 4, MULTIPLICATION: 5});
+
+class Token {
+    constructor(token) {
+        this.next = null;
+        this.previous = null;
+        this.tokenType = TokenType.VALUE; 
+        
+        if(token == "(") {
+            this.tokenType = TokenType.OPEN_PARANTHESES;
+        }
+        else if (token == ")") {
+            this.TokenType = TokenType.CLOSE_PARANTHESES;
+        }
+        else if (token == "+" || token == "-") {
+            this.tokenType = TokenType.ADDITION;
+        }
+        else if (token == "*" || token == "/") {
+            this.tokenType = TokenType.MULTIPLICATION;
+        }
+
+        this.value = token;
+    }
+}
+
+class TokenList {
+    constructor() {
+        this.head = null;
+        this.tail = null;  
+    }
+
+    add(token) {
+        if(this.head == null) {
+            this.head = token;
+        }
+        else {
+            this.tail.next = token;
+            token.previous = this.tail;
+        }
+        this.tail = token;
+    }
+}
+
+/**
+ * 
+ * @param {*} input 
+ * @return {TokenList}
+ */
+function tokenize(input) {
+    let tokenRetriever = /(\d+(\.\d+)?|[+\-*/x()])/; // This may need additional work. It fails for .32 or something similar (must do 0.32).
+    let tokenList = new TokenList(); 
+
+    while(input != "") {
+        let token = "";
+        input = input.replace(tokenRetriever, (matchedString, savedToken) => { token = savedToken; return "";});
+        tokenList.add(new Token(token));
+    }
+
+    return tokenList;
+}
+
+
+class TreeNode {
+    constructor() {
+        this.token = null;
+        this.leftChild = null;
+        this.rightChild = null;
+        this.parent = null;
+    }
+
+    createLeftChild() {
+        this.leftChild = new TreeNode();
+        this.leftChild.parent = this;
+    }
+
+    createRightChild() {
+        this.rightChild = new TreeNode();
+        this.rightChild.parent = this;
+    }
+}
+
+const ExpressionType = Object.freeze({EXP: 1, MEXP: 2, PEXP: 3});
+
+function findSplitToken(expressionType, startToken, endToken) {
+    let currentToken = endToken;
+
+    let tokenType = TokenType.ADDITION;
+
+    if(expressionType == ExpressionType.MEXP) {
+        tokenType = TokenType.MULTIPLICATION;
+    }
+
+    while(currentToken != startToken && currentToken != startToken.previous) {
+        if(currentToken.tokenType == tokenType) {
+            return currentToken;
+        }
+
+        currentToken = currentToken.previous;
+    }
+
+    return null;
+}
+
+/**
+ * 
+ * @param {TreeNode} root 
+ * @param {ExpressionType} expressionType 
+ * @param {Token} start
+ * @param {Token} end
+ */
+function createParseTreeNodes(root, expressionType, startToken, endToken) {
+    while(root.value == null) {
+        if (expressionType == ExpressionType.EXP) {
+            let aToken = findSplitToken(expressionType, startToken, endToken);
+            if(aToken == null) {
+                expressionType = ExpressionType.MEXP;
+            }
+            else {
+                root.createLeftChild();
+                root.createRightChild();
+                createParseTreeNodes(root.leftChild, ExpressionType.EXP, startToken, aToken.previous);
+                createParseTreeNodes(root.rightChild, ExpressionType.MEXP, aToken.next, endToken);
+                root.token = aToken;
+                return;
+            }
+        }
+        else if (expressionType == ExpressionType.MEXP) {
+            let mToken = findSplitToken(expressionType, startToken, endToken);
+            if (mToken == null) {
+                expressionType = ExpressionType.PEXP;
+            }
+            else {
+                root.createLeftChild();
+                root.createRightChild();
+                createParseTreeNodes(root.leftChild, ExpressionType.MEXP, startToken, mToken.previous);
+                createParseTreeNodes(root.rightChild, ExpressionType.PEXP, mToken.next, endToken);
+                root.token = mToken;
+                return;
+            }
+        }
+        else {
+            if (startToken.tokenType == TokenType.OPEN_PARANTHESES) {
+                expressionType = ExpressionType.EXP;
+                startToken = startToken.next;
+                endToken = endToken.previous;
+            }
+            else {
+                root.token = startToken;
+                return;
+            }
+        }
+    }
+}
+
+function createParseTree(input) {
+    let tokenList = tokenize(input);
+    let tree = new TreeNode();
+
+    createParseTreeNodes(tree, ExpressionType.EXP, tokenList.head, tokenList.tail);
+    console.log(printParseTree(tree));
+    return tree;
+}
+
+function printParseTree(tree) {
+    if(tree == null) {
+        return "";
+    }
+
+    return printParseTree(tree.leftChild) + tree.token.value + printParseTree(tree.rightChild);
+}
+
+/**
+ * 
+ * @param {TreeNode} tree 
+ * @param {*} x 
+ */
+function evaluateFunction(tree, x) {
+    let result;
+    if(tree.leftChild == null) {
+        if(tree.token.value == "x") {
+            return x;
+        }
+        result = tree.token.value;
+        return tree.token.value;
+    }
+    else if (tree.token.value == "*") {
+        let left = evaluateFunction(tree.leftChild, x);
+        let right = evaluateFunction(tree.rightChild, x);
+        result = Number(left) * Number(right);
+        return result;
+    }
+    else if (tree.token.value == "/") {
+        let numerator = Number(evaluateFunction(tree.leftChild, x));
+        let denominator = Number(evaluateFunction(tree.rightChild, x));
+        if(denominator == 0) {
+            if(numerator == 0) {
+                return undefined;
+            }
+            return Infinity * numerator;
+        }
+        return numerator / denominator;
+    }
+    else if (tree.token.value == "+") {
+        let left = evaluateFunction(tree.leftChild, x);
+        let right = evaluateFunction(tree.rightChild, x);
+        result = Number(left) + Number(right);
+        return result;
+    }
+    else { //if (tree.token.value == "-") {
+        result = Number(evaluateFunction(tree.leftChild, x)) - Number(evaluateFunction(tree.rightChild, x));
+        return result;
+    }
+}
+
+// IMPLEMENT PARANTHESE BALANCING!!!
